25cd5dca080e098f4cc8233ce3b42b3f916de1f6
Alan Mackay <alan.mackay810@gmail.com>
2016-02-14 15:43:15 -0800

    Gave credit where credit is due

--- GooMain.java
+++ GooMain.java
@@ -1,8 +1,8 @@
 // GooMain.java
 //
 // TEAM MEMBERS:
-//     * PUT FIRST TEAM MEMBER NAME HERE
-//     * PUT SECOND TEAM MEMBER NAME HERE
+//     * Alan Mackay
+//     * Daniel Gilroy
 //
 // Main program which invokes the various compiler phases:
 // lexer/parser, symbol table construction, type checking ...
